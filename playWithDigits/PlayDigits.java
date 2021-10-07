b723b349ed7f9838faefd26c51491ff29cd5135d
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2021-10-07 15:57:12 -0700

    Add files via upload

---
+++ playWithDigits/PlayDigits.java
@@ -0,0 +1,45 @@
+package digits;
+
+import digits.PlayDigits;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayDigits {
+
+    public PlayDigits() {
+    }
+
+    @Test
+    public void testDigPow89with1() {
+        System.out.println("digPow");
+        int n = 89;
+        int p = 1;
+        int expResult = 0;
+        int result = PlayDigits.digPow(n, p);
+        assertEquals(1, result);
+    }
+    
+    @Test
+    public void testDigPow92with2() {
+        System.out.println("digPow");
+        int n = 92;
+        int p = 2;
+        int expResult = 0;
+        int result = PlayDigits.digPow(n, p);
+        assertEquals(-1, result);
+    }
+    
+    @Test
+    public void testDigPow46288with3() {
+        System.out.println("digPow");
+        int n = 46288;
+        int p = 3;
+        int expResult = 0;
+        int result = PlayDigits.digPow(n, p);
+        assertEquals(51, result);
+    }
+}
