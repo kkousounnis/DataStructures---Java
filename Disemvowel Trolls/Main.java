0f25d061b573cd48d77da01d112f7220b88d3ac1
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-01-03 17:32:31 -0600

    Update Main.java

--- Disemvowel Trolls/Main.java
+++ Disemvowel Trolls/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static String disemvowel(String str) {
-      
-        // Code away...
+      //intialize answer  
+      String answer = "";
+        
+      //iterate through the string
+      for(int i=0;i<str.length();i++){
+        //check if it is vowel
+        if(!checkVowels(str.charAt(i))){
+          answer = answer + str.charAt(i);
+        }
+      }
+      return answer;
+    }
+    public static boolean checkVowels(char c){
+      //itialize every vowel  
+      String vowels = "aeiouAEIOU";
+        
+      if(vowels.contains(String.valueOf(c))){ 
+        return true;
+      }
+      return false;      
     }
 }
