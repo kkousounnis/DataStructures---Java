67e4b7f1bad361e24483979ff89f857cc7186529
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-02 16:50:16 +0300

    Update VehicleController.java

--- CountNumberCharString/src/main/java/com/mycompany/Controller/VehicleController.java
+++ CountNumberCharString/src/main/java/com/mycompany/Controller/VehicleController.java
@@ -1,4 +1,10 @@
 package com.mycompany.Controller;
 
+import com.mycompany.service.VehicleService;
+
 public class VehicleController {
+
+    public void Vehicle() {
+        new VehicleService().Vehicle();
+    }
 }
