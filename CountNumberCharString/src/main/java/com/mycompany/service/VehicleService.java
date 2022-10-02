6c1193ef715448381d7b23d527da08134bb06471
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-02 16:50:31 +0300

    Update VehicleService.java

--- CountNumberCharString/src/main/java/com/mycompany/service/VehicleService.java
+++ CountNumberCharString/src/main/java/com/mycompany/service/VehicleService.java
@@ -1,4 +1,15 @@
 package com.mycompany.service;
 
+import com.mycompany.model.Car;
+
 public class VehicleService {
+
+    public void Vehicle(){
+
+        Car car1 = new Car("Audi", "100000", "300");
+
+        car1.startCarEngine();
+        car1.stopCarEngine();
+        System.out.println(car1);
+    }
 }
