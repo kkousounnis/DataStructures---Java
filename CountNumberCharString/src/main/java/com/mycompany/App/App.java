39831b9eb92b3aca214dab92292ccf477f376185
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-02 15:13:59 +0300

    Update App.java

--- CountNumberCharString/src/main/java/com/mycompany/App/App.java
+++ CountNumberCharString/src/main/java/com/mycompany/App/App.java
@@ -1,15 +1,18 @@
 package com.mycompany.App;
 
 import com.mycompany.Controller.CountNumberCharStringController;
+import com.mycompany.Controller.DuplicateCharactersController;
 import com.mycompany.model.Sentence;
 
 public class App {
     /**
-     *
      * Main class
      */
     public static void main(String[] args) {
         //Write a Java Program to count the number of words in a string using HashMap.
-        new CountNumberCharStringController().countNumberCharString(new Sentence(""));
+        //new CountNumberCharStringController().countNumberCharString(new Sentence(""));
+
+        //Write a Java Program to find the duplicate characters in a string.
+        new DuplicateCharactersController().DuplicateCharacters(new Sentence(""));
     }
 }
