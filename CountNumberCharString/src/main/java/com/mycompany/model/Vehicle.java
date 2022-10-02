b0c4c5052a86d89e8df5c5060f0aa6bc6231f6bc
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-02 16:50:26 +0300

    Update Vehicle.java

--- CountNumberCharString/src/main/java/com/mycompany/model/Vehicle.java
+++ CountNumberCharString/src/main/java/com/mycompany/model/Vehicle.java
@@ -8,4 +8,31 @@ abstract class Vehicle {
 
     abstract void buy();
 
+    public Vehicle() {
+    }
+
+    public Vehicle(String name, String cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
 }
